f9767f7ad8ef8bc466eb0116c8f88ded6dfb0ab2
CosmiCloud <tybranha@gmail.com>
2023-06-26 20:41:05 -0400

    basic stats rough draft home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,87 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../css/home.css'
+import Loading from '../Loading'
+import axios from 'axios'
+let ext
 
-const home = () => {
+ext = 'http'
+if(process.env.REACT_APP_RUNTIME_HTTPS === 'true'){
+  ext = 'https'
+}
+
+const Home = () => {
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    async function fetchData () {
+      try {
+        const response = await axios.get(
+          `${ext}://${process.env.REACT_APP_RUNTIME_HOST}/home`
+        )
+        setData(response.data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    setData('')
+    fetchData()
+  }, [])
+
+  console.log(data)
   return (
     <div className='home'>
-      {/* Render the fetched data
-      {data.map(item => (
-        <p key={item.id}>{item.title}</p>
-      ))} */}
-
-      <header className='home-header'>
-        <p>home page</p>
-      </header>
+      {data ? (
+        <header className='home-header'>
+          <div className='home-form'>
+            <h1>Network Statistics</h1>
+            <div className='total-assets'>
+              Total Assets
+              <br></br>
+              <div className='home-stats-info'>
+                {data.pub_count}
+              </div>
+            </div>
+            <div className='trac-spent'>
+              Trac Spent
+              <br></br>
+              <div className='home-stats-info'>
+                {data.tracSpent.toFixed(2)}
+              </div>
+            </div>
+            <div className='assets-24h'>
+              Assets 24h
+              <br></br>
+              <div className='home-stats-info'>{data.v_pubs_stats_24h.totalPubs}</div>
+            </div>
+            <div className='trac-spent-24h'>
+              Spent 24h
+              <br></br>
+              <div className='home-stats-info'>
+              {data.v_pubs_stats_24h.totalTracSpent.toFixed(2)}
+              </div>
+            </div>
+            <div className='home-nodes'>
+              Nodes
+              <br></br>
+              <div className='home-stats-info'>
+              {data.v_nodes.length}
+              </div>
+            </div>
+            <div className='home-total-stake'>
+              Total Stake
+              <br></br>
+              <div className='home-stats-info'>
+                {Number(data.totalStake).toFixed(2)}
+              </div>
+            </div>
+          </div>
+        </header>
+      ) :  (
+        <Loading />
+      )}
     </div>
   )
 }
 
-export default home
+export default Home
