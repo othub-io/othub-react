929983475ad7e498c3f5dd50a064d66cf57b2ba3
CosmiCloud <tybranha@gmail.com>
2023-07-09 22:23:50 -0400

    More friendly page development message. footer links.

--- src/navigation/Footer.js
+++ src/navigation/Footer.js
@@ -5,7 +5,7 @@ const footer = () => {
   return (
     <div className='footer'>
       <header className='footer-header'>
-        <p>Built by the Origintrail Community. Open Sourced on Github.</p>
+        <p>Built by the Origintrail Community. Open Sourced on <a href='https://github.com/othub-io' >Github</a>. <a href='https://github.com/othub-io/othub-io/blob/main/Disclaimer.md' >Disclaimer</a>.</p>
       </header>
     </div>
   )
