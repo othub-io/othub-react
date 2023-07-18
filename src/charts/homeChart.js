7a240bf20bc82f3e3dd698775a790f7b37bd807d
CosmiCloud <tybranha@gmail.com>
2023-07-18 18:14:20 -0400

    added support for 1566px. replaced gauge with progress bar to 1m. change total assets to age of network.

--- src/charts/homeChart.js
+++ src/charts/homeChart.js
@@ -27,7 +27,26 @@ const HomeChart = ({ data }) => {
   data = JSON.parse(data)
   // Extract labels and data from the dataset
   const labels = data.map((item) => moment(item.date).format('DD MMM'));
+  // const expDate = data.map((item) => {
+  //   const days_to_expire = Number(item.epochs_number) * Number(item.epoch_length_days) * 24 * 60 * 60 * 1000
+
+  //   const date = new Date(item.block_ts_hour);
+  //   const mint_date = date.getTime();
+
+  //   const expire_date = mint_date + days_to_expire
+  //   const expire_date_fancy = new Date(expire_date).toISOString();
+  //   return expire_date_fancy
+  // });
+
   const pubCounts = data.map((item) => item.totalPubs);
+  // let expCounts = 0
+  // for(let i = 0; i < data.length;i++){
+  //   let item = data[i]
+  //   const days_to_expire = Number(item.epochs_number) * Number(item.epoch_length_days) * 24 * 60 * 60 * 1000
+  //   if(days_to_expire <= 0){
+  //     expCounts = expCounts + 1
+  //   }
+  // }
   
   console.group(pubCounts)
   const formattedData = {
@@ -39,6 +58,12 @@ const HomeChart = ({ data }) => {
         fill: false,
         borderColor: '#6168ED',
       },
+      // {
+      //   label: 'Expiring',
+      //   data: expCounts,
+      //   fill: false,
+      //   borderColor: '#000000',
+      // },
     ],
   };
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,13 +2,50 @@ import React, { useState, useEffect } from 'react'
 import '../css/home.css'
 import Loading from '../Loading'
 import HomeChart from '../charts/homeChart'
-import HomeGauge from '../charts/homeGauge'
 import axios from 'axios'
 let ext
 
 ext = 'http'
 if(process.env.REACT_APP_RUNTIME_HTTPS === 'true'){
   ext = 'https'
+}
+
+function formatNumber (number) {
+  if (number >= 1000) {
+    const suffixes = ['', 'K', 'M', 'B', 'T']
+    const suffixIndex = Math.floor(Math.log10(number) / 3)
+    const shortNumber =
+      suffixIndex !== 0 ? number / Math.pow(1000, suffixIndex) : number
+    const roundedNumber = Math.round(shortNumber * 10) / 10
+    return (
+      roundedNumber.toString().replace(/\.0$/, '') + suffixes[suffixIndex]
+    )
+  }
+  return number.toString()
+}
+
+function getReadableTime(days) {
+  let remainingDays = days;
+
+  const years = Math.floor(remainingDays / 365);
+  remainingDays -= years * 365;
+
+  const months = Math.floor(remainingDays / 30);
+  remainingDays -= months * 30;
+
+  let result = '';
+
+  if (years > 0) {
+    result += `${years}y ${years === 1 ? '' : ''}`;
+  }
+  if (months > 0) {
+    result += `${months}mo ${months === 1 ? '' : ''} `;
+  }
+  if (remainingDays > 0) {
+    result += `${remainingDays}d ${remainingDays === 1 ? '' : ''}`;
+  }
+
+  return result;
 }
 
 const Home = () => {
@@ -31,6 +68,28 @@ const Home = () => {
   }, [])
 
   console.log(data)
+  let pub_count
+  let tracSpent_24h
+  let totalPubs_24h
+  let tracSpent
+  let totalStake
+
+  if(data){
+    pub_count = formatNumber(parseFloat(data.pub_count))
+    tracSpent =  formatNumber(parseFloat(Number(data.totalTracSpent).toFixed(2)))
+    totalPubs_24h = formatNumber(parseFloat(data.totalPubs))
+    tracSpent_24h = formatNumber(parseFloat(data.tracSpent_24h).toFixed(2))
+    totalStake = formatNumber(parseFloat(data.totalStake))
+  }
+  
+
+  const percentage = (data.pub_count / 1000000) * 100;
+  const startDate = new Date("2022-12-14");
+    const currentDate = new Date();
+    const diffTime = Math.abs(currentDate - startDate);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    const timeSinceStartDate = getReadableTime(diffDays);
+
   return (
     <div className='home'>
       {data ? (
@@ -38,25 +97,37 @@ const Home = () => {
           <div className='header'>
           Network Statistics
           </div>
-
-          <div className='home-gauge'>
-              <HomeGauge data={JSON.stringify(data.totalPubs_24h)}/>
+          <div className="bar-header">
+            <div className="bar-title">
+              Road to 1 Million Assets
             </div>
-            <div className="pub-o-meter">Pub-o-Meter</div>
-            <div className="gauge-max">5,000</div>
+            <div className="bar-legend">
+              500K
+            </div>
+            <div className="bar-legend">
+              1M
+            </div>
+            <div className="progress-bar">
+              <div className="progress-bar-fill" style={{ width: `${percentage}%` }}>
+                  <div className='bar-text'>
+                    {pub_count}
+                  </div>
+                </div>
+            </div>
+          </div>
           <div className='home-form'>
             <div className='total-assets'>
-              Total Assets
+              Age
               <br></br>
               <div className='home-stats-info'>
-                {data.pub_count}
+                {timeSinceStartDate}
               </div>
             </div>
             <div className='home-total-stake'>
               Total Stake
               <br></br>
               <div className='home-stats-info'>
-                {data.totalStake}
+                {totalStake}
               </div>
             </div>
             <div className='home-nodes'>
@@ -70,19 +141,19 @@ const Home = () => {
               Total Trac Spent
               <br></br>
               <div className='home-stats-info'>
-                {data.tracSpent}
+                {tracSpent}
               </div>
             </div>
             <div className='assets-24h'>
               Assets 24h
               <br></br>
-              <div className='home-stats-info'>{data.totalPubs_24h}</div>
+              <div className='home-stats-info'>{totalPubs_24h}</div>
             </div>
             <div className='trac-spent-24h'>
               Trac Spent 24h
               <br></br>
               <div className='home-stats-info'>
-              {data.tracSpent_24h}
+              {tracSpent_24h}
               </div>
             </div>
             <div className='home-chart'>
