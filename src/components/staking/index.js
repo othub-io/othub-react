b8db7fb6d62d1e80bd683a8278f570b08c03d960
CosmiCloud <tybranha@gmail.com>
2024-02-18 13:54:38 -0500

    staking page edits. added socials. added sync warning

--- src/components/staking/index.js
+++ src/components/staking/index.js
@@ -10,47 +10,49 @@ const Staking = () => {
         {/* <NetworkDrop network={setNetwork} blockchain={setBlockchain} /> */}
       </div>
       <div className="staking-body">
-        <div className="staking-about">
-          <h1>About OTHub</h1>
-          <p>
-            OThub.io is a free, open-source community-driven web tool that
-            democratizes access to the Origintrail Decentralized Knowledge
-            Graph. Initially created in 2018 by a community member Luke, OThub
-            has evolved significantly since the introduction of Origintrail DKG
-            V6. The current OTHub team is comprised of four members - Dmitry,
-            BRX, Cosmi, with Luke serving as an advisor with the support of
-            Trace Labs' ChatDKG grant program. OTHub now supports a broader
-            range of use cases than its predecessor, offering features like
-            network statistics, asset catalogs, network analytics, knowledge
-            asset publishing, and node operator insights. Additionally, OTHub
-            provides a free developer API to encourage and support knowledge app
-            development.
-            <br />
-            <br />
-            OTHub actively operates Origintrail nodes across all supported
-            testnet and mainnet networks, underpinning our web infrastructure.
-            Our ambition is to become the preferred host for reliable community
-            nodes, channeling rewards back into OTHub and its community.
-            Additionally, the operator fee on nodes hosted by OTHub will be used
-            to cover OTHub operating expenses as well as OTHub improvement PR
-            rewards and funding the mainnet API. The OTHub team has committed
-            many hours to create and maintain OTHub and our goal is to keep
-            providing our services to the community for free.
-          </p>
+        <div className="about-title">About OTHub</div>
+        <div className="about-text">
+          OThub.io is a free, open-source community-driven web tool that
+          democratizes access to the Origintrail Decentralized Knowledge Graph.
+          Initially created in 2018 by a community member Luke, OThub has
+          evolved significantly since the introduction of Origintrail DKG V6.
+          The current OTHub team is comprised of four members - Dmitry, BRX,
+          Cosmi, with Luke serving as an advisor with the support of Trace Labs'
+          ChatDKG grant program. OTHub now supports a broader range of use cases
+          than its predecessor, offering features like network statistics, asset
+          catalogs, network analytics, knowledge asset publishing, and node
+          operator insights. Additionally, OTHub provides a free developer API
+          to encourage and support knowledge app development.
+          <br />
+          <br />
+          OTHub actively operates Origintrail nodes across all supported testnet
+          and mainnet networks, underpinning our web infrastructure. Our
+          ambition is to become the preferred host for reliable community nodes,
+          channeling rewards back into OTHub and its community. Additionally,
+          the operator fee on nodes hosted by OTHub will be used to cover OTHub
+          operating expenses as well as OTHub improvement PR rewards and funding
+          the mainnet API. The OTHub team has committed many hours to create and
+          maintain OTHub and our goal is to keep providing our services to the
+          community for free.
         </div>
-        <h2>Offical OTHub Nodes</h2>
-        <div className="staking-node-list">
+        <div className="nodes-title">Offical OTHub Nodes</div>
         <Stats
-              data={[
-                {
-                  network: "DKG Mainnet",
-                  blockchain: "",
-                },
-              ]}
-            />
+          data={[
+            {
+              network: "DKG Mainnet",
+              blockchain: "",
+            },
+          ]}
+        />
+        <div className="delegate-button">
+          <a
+            href="https://dkg.origintrail.io/staking"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Delegate
+          </a>
         </div>
-        <br></br>
-        <a href="https://dkg.origintrail.io/staking" target="_blank"  rel="noreferrer">Delegate</a>
       </div>
     </div>
   );
